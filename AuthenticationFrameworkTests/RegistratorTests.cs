15cbc38948c2d529f888126879069bc51d7304e1
noname <noname@nomail>
2024-04-05 16:50:25 +0200

    Implemented tests

--- AuthenticationFrameworkTests/RegistratorTests.cs
+++ AuthenticationFrameworkTests/RegistratorTests.cs
@@ -9,7 +9,7 @@ namespace AuthenticationFrameworkTests;
 
 public sealed class RegistratorTests
 {
-    readonly IDbContextFactory<TestingDbContext> _dbContextFactory;
+    readonly TestingDbContextFactory _dbContextFactory;
     readonly IUserManager<User, Role> _userManager;
 
     public RegistratorTests()
@@ -17,6 +17,7 @@ public sealed class RegistratorTests
         _dbContextFactory = new TestingDbContextFactory(
             new DbContextOptionsBuilder()
                 .UseSqlite("Datasource=RegistratorTestsDatabase")
+                .EnableSensitiveDataLogging()
                 .Options);
 
         _userManager = new UserManagerBuilder<User, Role>()
@@ -120,6 +121,23 @@ public sealed class RegistratorTests
     }
 
     [Fact]
+    public void Register_UserWithExistingUserName_ThrowInvalidUsernameException()
+    {
+        //arrange
+        User newUser = new()
+        {
+            UserName = "admin",
+        };
+        string password = "newpass!01";
+
+        //act
+        Action act = () => _userManager.Registrator.Register(newUser, password);
+
+        //assert
+        act.Should().Throw<InvalidUserException>();
+    }
+
+    [Fact]
     public void Register_UserWithInvalidPassword_ThrowInvalidPasswordException()
     {
         //arrange
@@ -182,4 +200,181 @@ public sealed class RegistratorTests
             .Should()
             .Equal(["admin", "Carl101", "JohnDear56", "Pam51"]);
     }
+
+    [Fact]
+    public async Task ChangePasswordAsync_User_CanLoginWithNewPassword()
+    {
+        //arrange
+        string userName = "admin";
+        string password = "admin!01";
+        string newPassword = "newpassword!01";
+        bool isLogged = _userManager.Logger.Login(userName, password);
+        _userManager.Logger.Logout();
+
+        //act
+        await _userManager.Registrator.ChangePasswordAsync(userName, newPassword);
+        bool isLoggedWithNewPass = _userManager.Logger.Login(userName, newPassword);
+        _userManager.Logger.Logout();
+
+        //assert
+        isLogged.Should().BeTrue();
+        isLoggedWithNewPass.Should().BeTrue();
+
+        //finally
+        _dbContextFactory
+            .CreateDbContext()
+            .Database
+            .EnsureDeleted();
+    }
+
+    [Fact]
+    public async Task ChangePasswordAsync_ToInvalidUser_ThrowInvalidUserException()
+    {
+        //arrange
+        string userName = "wrong";
+        string newPassword = "newpassword!01";
+
+        //act
+        Func<Task> act = async () => await _userManager.Registrator.ChangePasswordAsync(userName, newPassword);
+
+        //assert
+        await act.Should().ThrowAsync<InvalidUserException>();
+    }
+
+    [Fact]
+    public async Task ChangePasswordAsync_UsingInvalidPassword_ThrowInvalidPasswordException()
+    {
+        //arrange
+        string userName = "admin";
+        string newPassword = "wrong";
+
+        //act
+        Func<Task> act = async () => await _userManager.Registrator.ChangePasswordAsync(userName, newPassword);
+
+        //assert
+        await act.Should().ThrowAsync<InvalidPasswordException>();
+    }
+
+    [Fact]
+    public async Task RegisterAsync_NewUser_CanLogin()
+    {
+        //arrange
+        User newUser = new()
+        {
+            UserName = "newUser",
+        };
+        string password = "newpass!01";
+
+        //act
+        await _userManager.Registrator.RegisterAsync(newUser, password);
+        bool isLogged = _userManager.Logger.Login(newUser.UserName, password);
+
+        //assert
+        isLogged.Should().BeTrue();
+
+        //finally
+        _dbContextFactory
+            .CreateDbContext()
+            .Database
+            .EnsureDeleted();
+    }
+
+    [Fact]
+    public async Task RegisterAsync_UserWithInvalidUserName_ThrowInvalidUsernameException()
+    {
+        //arrange
+        User newUser = new()
+        {
+            UserName = string.Empty,
+        };
+        string password = "newpass!01";
+
+        //act
+        Func<Task> act = async () => await _userManager.Registrator.RegisterAsync(newUser, password);
+
+        //assert
+        await act.Should().ThrowAsync<InvalidUserException>();
+    }
+
+    [Fact]
+    public async Task RegisterAsync_UserWithExistingUserName_ThrowInvalidUsernameException()
+    {
+        //arrange
+        User newUser = new()
+        {
+            UserName = "admin",
+        };
+        string password = "newpass!01";
+
+        //act
+        Func<Task> act = async () => await _userManager.Registrator.RegisterAsync(newUser, password);
+
+        //assert
+        await act.Should().ThrowAsync<InvalidUserException>();
+    }
+
+    [Fact]
+    public async Task RegisterAsync_UserWithInvalidPassword_ThrowInvalidPasswordException()
+    {
+        //arrange
+        User newUser = new()
+        {
+            UserName = "newUser",
+        };
+        string password = "wrong";
+
+        //act
+        Func<Task> act = async () => await _userManager.Registrator.RegisterAsync(newUser, password);
+
+        //assert
+        await act.Should().ThrowAsync<InvalidPasswordException>();
+    }
+
+    [Fact]
+    public async Task UnregisterAsync_User_RemovesItFromDatabase()
+    {
+        //arrange
+        var context = _dbContextFactory.CreateDbContext();
+        var userName = "Pam51";
+
+        //act
+        bool pamExsists = context.Users.Any(user => user.UserName == userName);
+        await _userManager.Registrator.UnregisterAsync(userName);
+        bool pamExsistsAfter = context.Users.Any(user => user.UserName == userName);
+
+        //assert
+        pamExsists.Should().BeTrue();
+        pamExsistsAfter.Should().BeFalse();
+
+        //finally
+        context.Database.EnsureDeleted();
+    }
+
+    [Fact]
+    public async Task UnregisterAsync_InvalidUser_ThorwInvalidUserException()
+    {
+        //arrange
+        var userName = "wrong";
+
+        //act
+        Func<Task> act = async () => await _userManager.Registrator.UnregisterAsync(userName);
+
+        //assert
+        await act.Should().ThrowAsync<InvalidUserException>();
+    }
+
+    [Fact]
+    public async Task GetUsersAsync_ShouldContainAllTheUsers()
+    {
+        //act
+        var users = await _userManager.Registrator.GetUsersAsync();
+
+        //assert
+        users
+            .Select(user => user.UserName)
+            .Order()
+            .Should()
+            .Equal(["admin", "Carl101", "JohnDear56", "Pam51"]);
+    }
+
 }
---
+++ AuthenticationFrameworkTests/SubscriberTests.cs
@@ -0,0 +1,345 @@
+﻿using Authentication;
+using Authentication.Exceptions;
+using Authentication.Interfaces;
+using AuthenticationFrameworkTests._Global;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationFrameworkTests;
+
+public sealed class SubscriberTests
+{
+    readonly TestingDbContextFactory _dbContextFactory;
+    readonly IUserManager<User, Role> _userManager;
+
+    public SubscriberTests()
+    {
+        _dbContextFactory = new TestingDbContextFactory(
+            new DbContextOptionsBuilder()
+                .UseSqlite("Datasource=SubscriberTestsDatabase")
+                .EnableSensitiveDataLogging()
+                .Options);
+
+        _userManager = new UserManagerBuilder<User, Role>()
+            .UseDbContextStore(_dbContextFactory)
+            .CreateUserManager();
+    }
+
+    [Fact]
+    public void Roles_Returns_AllRoles()
+    {
+        //act
+        var roles = _userManager.Subscriber.Roles;
+
+        //assert
+        roles
+            .Select(role => role.RoleKey)
+            .Order()
+            .Should()
+            .Equal(["ADMIN", "GUEST", "USER"]);
+    }
+
+    [Fact]
+    public void GetUserRoles_Returns_AllTheRolesSubscribed()
+    {
+        //arrange
+        string user = "Pam51";
+        string[] expectedSubscriptions = ["GUEST", "USER"];
+
+        //act
+        var subscriptions = _userManager.Subscriber.GetUserRoles(user);
+
+        //assert
+        subscriptions
+            .Select(role => role.RoleKey)
+            .Order()
+            .Should()
+            .Equal(expectedSubscriptions);
+    }
+
+    [Fact]
+    public void IsSubscribed_UserSubscribedToRole_ShouldBeTrue()
+    {
+        //arrange
+        string user = "Pam51";
+        string role = "USER";
+        string extraRole = "ADMIN";
+
+        //act
+        bool isSubscribed = _userManager.Subscriber.IsSubscribed(user, role, extraRole);
+
+        //assert
+        isSubscribed.Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsSubscribed_UserNotSubscribedToRole_ShouldBeFalse()
+    {
+        //arrange
+        string user = "Pam51";
+        string role = "ADMIN";
+
+        //act
+        bool isSubscribed = _userManager.Subscriber.IsSubscribed(user, role);
+
+        //assert
+        isSubscribed.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Subscribe_UserIsSubscribed_ShouldBeTrue()
+    {
+        //arrange
+        string user = "Pam51";
+        string addedRole = "ADMIN";
+
+        //act
+        _userManager.Subscriber.Subscribe(addedRole, user);
+        bool isSubscribed = _userManager.Subscriber.IsSubscribed(user, addedRole);
+
+        //assert
+        isSubscribed.Should().BeTrue();
+
+        //finally
+        _dbContextFactory
+            .CreateDbContext()
+            .Database
+            .EnsureDeleted();
+    }
+
+    [Fact]
+    public void Subscribe_UsingInvalidUser_ShouldThrowInvalidUserException()
+    {
+        //arrange
+        string user = "wrong";
+        string addedRole = "ADMIN";
+
+        //act
+        Action act = () => _userManager.Subscriber.Subscribe(addedRole, user);
+
+        //assert
+        act.Should().Throw<InvalidUserException>();
+    }
+
+    [Fact]
+    public void Subscribe_UsingInvalidRole_ShouldThrowInvalidRoleException()
+    {
+        //arrange
+        string user = "Pam51";
+        string addedRole = "wrong";
+
+        //act
+        Action act = () => _userManager.Subscriber.Subscribe(addedRole, user);
+
+        //assert
+        act.Should().Throw<InvalidRoleException>();
+    }
+
+    [Fact]
+    public void Unsubscribe_UserIsSubscribed_ShouldBeFalse()
+    {
+        //arrange
+        string user = "Pam51";
+        string removedRole = "USER";
+
+        //act
+        _userManager.Subscriber.Unsubscribe(removedRole, user);
+        bool isSubscribed = _userManager.Subscriber.IsSubscribed(user, removedRole);
+
+        //assert
+        isSubscribed.Should().BeFalse();
+
+        //finally
+        _dbContextFactory
+            .CreateDbContext()
+            .Database
+            .EnsureDeleted();
+    }
+
+    [Fact]
+    public void Unubscribe_UsingInvalidUser_ShouldThrowInvalidUserException()
+    {
+        //arrange
+        string user = "wrong";
+        string addedRole = "ADMIN";
+
+        //act
+        Action act = () => _userManager.Subscriber.Unsubscribe(addedRole, user);
+
+        //assert
+        act.Should().Throw<InvalidUserException>();
+    }
+
+    [Fact]
+    public void Unubscribe_UsingInvalidRole_ShouldThrowInvalidRoleException()
+    {
+        //arrange
+        string user = "Pam51";
+        string addedRole = "wrong";
+
+        //act
+        Action act = () => _userManager.Subscriber.Unsubscribe(addedRole, user);
+
+        //assert
+        act.Should().Throw<InvalidRoleException>();
+    }
+
+    [Fact]
+    public async Task GetRolesAsync_Returns_AllRoles()
+    {
+        //act
+        var roles = await _userManager.Subscriber.GetRolesAsync();
+
+        //assert
+        roles
+            .Select(role => role.RoleKey)
+            .Order()
+            .Should()
+            .Equal(["ADMIN", "GUEST", "USER"]);
+    }
+
+    [Fact]
+    public async Task GetUserRolesAsync_Returns_AllTheRolesSubscribed()
+    {
+        //arrange
+        string user = "Pam51";
+        string[] expectedSubscriptions = ["GUEST", "USER"];
+
+        //act
+        var subscriptions = await _userManager.Subscriber.GetUserRolesAsync(user);
+
+        //assert
+        subscriptions
+            .Select(role => role.RoleKey)
+            .Order()
+            .Should()
+            .Equal(expectedSubscriptions);
+    }
+
+    [Fact]
+    public async Task IsSubscribedAsync_UserSubscribedToRole_ShouldBeTrue()
+    {
+        //arrange
+        string user = "Pam51";
+        string role = "USER";
+        string extraRole = "ADMIN";
+
+        //act
+        bool isSubscribed = await _userManager.Subscriber.IsSubscribedAsync(user, role, extraRole);
+
+        //assert
+        isSubscribed.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task IsSubscribedAsync_UserNotSubscribedToRole_ShouldBeFalse()
+    {
+        //arrange
+        string user = "Pam51";
+        string role = "ADMIN";
+
+        //act
+        bool isSubscribed = await _userManager.Subscriber.IsSubscribedAsync(user, role);
+
+        //assert
+        isSubscribed.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task SubscribeAsync_UserIsSubscribed_ShouldBeTrue()
+    {
+        //arrange
+        string user = "Pam51";
+        string addedRole = "ADMIN";
+
+        //act
+        await _userManager.Subscriber.SubscribeAsync(addedRole, user);
+        bool isSubscribed = _userManager.Subscriber.IsSubscribed(user, addedRole);
+
+        //assert
+        isSubscribed.Should().BeTrue();
+
+        //finally
+        _dbContextFactory
+            .CreateDbContext()
+            .Database
+            .EnsureDeleted();
+    }
+
+    [Fact]
+    public async Task SubscribeAsync_UsingInvalidUser_ShouldThrowInvalidUserException()
+    {
+        //arrange
+        string user = "wrong";
+        string addedRole = "ADMIN";
+
+        //act
+        var act = async () => await _userManager.Subscriber.SubscribeAsync(addedRole, user);
+
+        //assert
+        await act.Should().ThrowAsync<InvalidUserException>();
+    }
+
+    [Fact]
+    public async Task SubscribeAsync_UsingInvalidRole_ShouldThrowInvalidRoleException()
+    {
+        //arrange
+        string user = "Pam51";
+        string addedRole = "wrong";
+
+        //act
+        var act = async () => await _userManager.Subscriber.SubscribeAsync(addedRole, user);
+
+        //assert
+        await act.Should().ThrowAsync<InvalidRoleException>();
+    }
+
+    [Fact]
+    public async Task UnsubscribeAsync_UserIsSubscribed_ShouldBeFalse()
+    {
+        //arrange
+        string user = "Pam51";
+        string removedRole = "USER";
+
+        //act
+        await _userManager.Subscriber.UnsubscribeAsync(removedRole, user);
+        bool isSubscribed = _userManager.Subscriber.IsSubscribed(user, removedRole);
+
+        //assert
+        isSubscribed.Should().BeFalse();
+
+        //finally
+        _dbContextFactory
+            .CreateDbContext()
+            .Database
+            .EnsureDeleted();
+    }
+
+    [Fact]
+    public async Task UnubscribeAsync_UsingInvalidUser_ShouldThrowInvalidUserException()
+    {
+        //arrange
+        string user = "wrong";
+        string addedRole = "ADMIN";
+
+        //act
+        var act = async () => await _userManager.Subscriber.UnsubscribeAsync(addedRole, user);
+
+        //assert
+        await act.Should().ThrowAsync<InvalidUserException>();
+    }
+
+    [Fact]
+    public async Task UnubscribeAsync_UsingInvalidRole_ShouldThrowInvalidRoleException()
+    {
+        //arrange
+        string user = "Pam51";
+        string addedRole = "wrong";
+
+        //act
+        var act = async () => await _userManager.Subscriber.UnsubscribeAsync(addedRole, user);
+
+        //assert
+        await act.Should().ThrowAsync<InvalidRoleException>();
+    }
+}
